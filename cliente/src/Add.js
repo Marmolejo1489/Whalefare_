439ff189a9babbead50bb2420330187b249928a9
CarapiaOwO <jcarapiaa1900@alumno.ipn.mx>
2021-11-01 13:36:20 -0600

    Titulo para las contraseñas

--- cliente/src/Add.js
+++ cliente/src/Add.js
@@ -6,12 +6,13 @@ function Add() {
   const [Password, setPassword] = useState("");
   const [User, setUser] = useState("");
   const [Domain, setDomain] = useState("");
+  const [Title, setTitle] = useState("");
 
   const addPassword = () => {
     Axios.get('http://localhost:4000/login').then((response) => {
       if (response.data.user) {
         var id_u = response.data.user[0].id_u;
-        
+
         Axios.post("http://localhost:4000/add", {
           user_c: User,
           pass_c: Password,
@@ -20,17 +21,22 @@ function Add() {
         });
       }
     });
-
-
   };
 
   return (
-
-
     <div className="col">
       <div className="card text-center">
         <div className="container">
-          <label>Nombre de usuario</label>
+          <label>Título</label>
+          <div className="input-container">
+            <div class="mb-3 input-group">
+              <input type="text" id="formFileMultiple" className="form-control" placeholder="Escribe aquí." multiple
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                }} />
+            </div>
+          </div>
+          <label>Usuario</label>
           <div className="input-container">
             <input type="text" id="user" className="text_area" placeholder="Opcional"
               onChange={(event) => {
@@ -39,10 +45,14 @@ function Add() {
           </div>
           <label>Contraseña</label>
           <div className="input-container">
-            <input type="text" id="pass" className="text_area" placeholder="Escribe aquí."
-              onChange={(event) => {
-                setPassword(event.target.value);
-              }} />
+            <div className="mb-3 input-group">
+              <input type="text" id="formFileMultiple" class="form-control" placeholder="Escribe aquí." multiple
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }} />
+              <button class="input-group-text" id="basic-addon3">Eye</button>
+              <button class="input-group-text" id="basic-addon3">Key</button>
+            </div>
           </div>
           <label>Sitio web</label>
           <div className="input-container">
@@ -58,7 +68,6 @@ function Add() {
         </div>
       </div>
     </div>
-
   );
 }
 
