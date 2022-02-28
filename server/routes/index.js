0285637b1acb2e0530129d6215ba654ecd5bb832
CarapiaOwO <jcarapiaa1900@alumno.ipn.mx>
2022-02-27 21:44:06 -0600

    Index redirect

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ const path = require('path')
 router.get('/', (request, response) => {
     console.log("/")
     response.send({Prueba: 'Prueba?'})
+    response.redirect('https://whalefare.netlify.app')
 });
 
 module.exports = router;
