1b7d4fb57fe19a747fe516840511068015b151c4
CarapiaOwO <jcarapiaa1900@alumno.ipn.mx>
2022-02-27 21:54:17 -0600

    Index redirect fixed

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const path = require('path')
 
 router.get('/', (request, response) => {
-    console.log("/")
-    response.send({Prueba: 'Prueba?'})
     response.redirect('https://whalefare.netlify.app')
 });
 
